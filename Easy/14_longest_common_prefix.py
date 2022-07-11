fd5b0f46a5fdbc497944b0617b77e2fdd4fa633f
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-11 11:17:38 +0530

    Update 14_longest_common_prefix.py

--- Easy/14_longest_common_prefix.py
+++ Easy/14_longest_common_prefix.py
@@ -1,1 +1,13 @@
 
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_ele=min(strs,key=len)
+        subst=""
+        for i in range(len(min_ele)):
+            for j in range(len(strs)):
+                if min_ele[i]!=strs[j][i]:
+                    return subst
+            subst+=min_ele[i]
+        return subst
+
+        
