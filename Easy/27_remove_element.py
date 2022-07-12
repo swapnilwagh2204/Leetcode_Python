87b7c3aab80239ba55237d03d1b546036bcf775a
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-12 23:35:38 +0530

    Update 27_remove_element.py

--- Easy/27_remove_element.py
+++ Easy/27_remove_element.py
@@ -1,1 +1,10 @@
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ct=nums.count(val)
+        for i in range(ct):
+            nums.remove(val)
+        ln=len(nums)
+        for i in range(ct):
+            nums.append("_")
+        return ln
