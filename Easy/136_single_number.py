8f8f0456132547de2978f4a36034131d9dc76aaf
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-14 23:47:26 +0530

    Update 136_single_number.py

--- Easy/136_single_number.py
+++ Easy/136_single_number.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dt={}
+        for i in nums:
+            if i not in dt:
+                dt[i]=1
+            else:
+                dt[i]+=1
+        for i in dt:
+            if dt[i]==1:
+                return i
+            
+                
+        
