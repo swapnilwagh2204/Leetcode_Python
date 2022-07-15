d9bbaecb40c52ce7f9070198526ee2b6351544d3
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-15 21:49:10 +0530

    Update 169_majority_element.py

--- Easy/169_majority_element.py
+++ Easy/169_majority_element.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=int(len(nums)/2)
+        dt={}
+        for i in nums:
+            if i not in dt:
+                dt[i]=1
+            else:
+                dt[i]+=1
+        for i in dt:
+            if dt[i]>n:
+                return i
+                
+        
