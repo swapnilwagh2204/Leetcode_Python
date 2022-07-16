704102d0a65e78de3aa5aa226b07cadb0fadf7e2
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-16 18:35:32 +0530

    Update 217_contains_duplicate.py

--- Easy/217_contains_duplicate.py
+++ Easy/217_contains_duplicate.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dt={}
+        for i in nums:
+            if i not in dt:
+                dt[i]=1
+            else:
+                dt[i]+=1
+        for i in dt:
+            if dt[i]>=2:
+                return True
+        return False
+        
