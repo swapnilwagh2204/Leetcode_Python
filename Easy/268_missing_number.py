94f02fabde5c39ac4deebd7ec63a15b98c1a9036
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-17 14:21:55 +0530

    Update 268_missing_number.py

--- Easy/268_missing_number.py
+++ Easy/268_missing_number.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        max_n=max(nums)
+        for i in range(max_n):
+            if i not in nums:
+                return i
+        return len(nums)
+        
