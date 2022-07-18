8ff52908ef75b14da8641e0a3845cd88e42ea317
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-18 22:39:21 +0530

    Update 283_move_zeros.py

--- Easy/283_move_zeros.py
+++ Easy/283_move_zeros.py
@@ -1,1 +1,11 @@
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        ct=nums.count(0)
+        for i in range(ct):
+            nums.remove(0)
+        nums.extend([0]*ct)
+        return nums
