7b2d9df5c655cfe2c97967bd038b2e9c578974cb
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-20 18:58:33 +0530

    Update 448_Find All Numbers Disappeared_in_an_Array.py

--- Easy/448_Find All Numbers Disappeared_in_an_Array.py
+++ Easy/448_Find All Numbers Disappeared_in_an_Array.py
@@ -7,3 +7,11 @@ class Solution:
             if i not in nums:
                 not_in_nums.append(i)
         return not_in_nums
+
+    
+#     optimised_approach:
+
+    nums1={i for i in range(1,len(nums)+1)}
+    op=nums1-set(nums)
+    return op
+        
