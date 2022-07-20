d7382e49eba105ce2f02131eed9d58001ce533e2
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-20 18:57:59 +0530

    non optimised force appraoch

--- Easy/448_Find All Numbers Disappeared_in_an_Array.py
+++ Easy/448_Find All Numbers Disappeared_in_an_Array.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        not_in_nums=[]
+        n=len(nums)
+        for i in range(1,n+1):
+            if i not in nums:
+                not_in_nums.append(i)
+        return not_in_nums
