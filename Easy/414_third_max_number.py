598d8ce2b374498dad22337ad11b1c35bbcbfcc0
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-22 17:11:36 +0530

    Update 414_third_max_number.py

--- Easy/414_third_max_number.py
+++ Easy/414_third_max_number.py
@@ -1,1 +1,10 @@
 
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=list(set(nums))
+        nums.sort()
+        if len(nums)<3:
+            return max(nums)
+        else:
+            return nums[-3]
+        
