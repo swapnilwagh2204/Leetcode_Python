e14e2e7ec00b6018a2a7d86747db9ea421d4a3a4
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-25 12:57:10 +0530

    Update 219_contains_duplicate.py

--- Easy/219_contains_duplicate.py
+++ Easy/219_contains_duplicate.py
@@ -1,1 +1,8 @@
 
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                return True
+        return False
