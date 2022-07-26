a32d326dadcbed0326157947b64cf6961905ff2a
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-26 23:57:54 +0530

    Update 496_next_greater_element_1.py

--- Easy/496_next_greater_element_1.py
+++ Easy/496_next_greater_element_1.py
@@ -1,1 +1,23 @@
 
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        op=[]
+        for i in nums1:
+            if i in nums2:
+                ind=nums2.index(i)
+                ls=nums2[ind+1:]
+                if len(ls)>0:
+                    mx=max(ls)
+                    if mx>i:
+                        op.append(nums2[ind+1])
+                    else:
+                        op.append(-1)
+                else:
+                    op.append(-1)
+            else:
+                op.append(-1)
+
+        return op
+
+
+
