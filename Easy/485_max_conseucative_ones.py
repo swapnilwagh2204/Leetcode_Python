7c9bbcdebc78530c5d24d5f2d5a0fd12e56c02f3
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2022-07-27 00:09:50 +0530

    Update 485_max_conseucative_ones.py

--- Easy/485_max_conseucative_ones.py
+++ Easy/485_max_conseucative_ones.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ls=[]
+        count=0
+        op=0
+        for i in nums:
+            if i==1:
+                count+=1
+            else:
+                count=0
+            op=max(count,op)
+        return op
+      
+      
+      
